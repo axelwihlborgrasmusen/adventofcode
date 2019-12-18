5cdb03320751d8b791fd81d4b65eb0f1911ee308
Axel Wihlborg <axel.wihlborg@telavox.com>
2019-12-18 16:35:12 +0100

    Almost done p1d3

--- Day3/day3.c
+++ Day3/day3.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 struct coordinates {
 	int x;
 	int y;
 };
 
-struct coordinates createNewStruct(int x, int y) {
-        struct coordinates cord = {
-                .x = x, .y=y
-        };
+struct coordinates createNewCord(int x, int y) {
+	struct coordinates cord = {
+		.x = x, .y=y
+	};
 	return cord;
 }
 
-struct coordinates calculateNewCoordinates(struct coordinates* cablePath, char c, int i, struct coordinate lastPoint) {
-switch(c) {
-case 'R':
-	break; 
-		
+struct coordinates calculateNewCoordinates(char c, int i, struct coordinates lastPoint) {
+	struct coordinates newCord;
+	switch(c) {
+		case 'R':
+			newCord = createNewCord((lastPoint.x + i), lastPoint.y);
+			break;
+		case 'L':
+			newCord = createNewCord((lastPoint.x - i), lastPoint.y);
+			break;
+		case 'U':
+			newCord = createNewCord(lastPoint.x, (lastPoint.y + i));
+			break;
+		case 'D':
+			newCord = createNewCord(lastPoint.x, (lastPoint.y - i));
+			break;	
+	}
+	return newCord;
 }
+
+int calculateClosestIntersect(struct coordinates* cablePath, struct coordinates from, struct coordinates to) {
+
+	//calculate if there is an intersection here and return the length to that, else return max_int
+
+	return 0;
 }
 
 int readInput(FILE* file, struct coordinates* cablePath) {
-
 	if(file == NULL){
 		printf("Could not open specified file");
 		return -1;
@@ -32,30 +51,44 @@ int readInput(FILE* file, struct coordinates* cablePath) {
 	int firstCable = 1;
 
 	while(fscanf(file, "%c%d,", &c, &i)>0) {
-		if(firstCable == 1) {		
 			if(c != '\n'){
 				steps++;
 			} else {
-				firstCable = 0;
+				goto nextStep;
 			}
+	}
+	nextStep:
+	cablePath = malloc((steps+1) * sizeof(struct coordinates));
+	rewind(file);
+
+	struct coordinates lastPoint = createNewCord(0,0);
+	struct coordinates newPoint = createNewCord(0,0);
+	int step = 0;	
+	cablePath[step] = lastPoint;
+	int closestIntersect = INT_MAX;
+	int intersect = INT_MAX;	
+
+	while(fscanf(file, "%c%d,", &c, &i)>0) {
+		if(firstCable == 1) {
+			if(c != '\n'){
+				step++;
+				lastPoint = calculateNewCoordinates(c, i, lastPoint);
+				cablePath[step] = lastPoint;
+			} else {
+				firstCable = 0;
+				lastPoint = createNewCord(0,0);
+			}
+		} else {
+			newPoint = calculateNewCoordinates(c, i, lastPoint);
+			intersect = calculateClosestIntersect(cablePath, lastPoint, newPoint);
+			if(intersect < closestIntersect) {
+				closestIntersect = intersect;
+			}
+			lastPoint = newPoint;
 		}
 	}
-	
-	cablePath = malloc((steps+1) * sizeOf(coordinates));
-	rewindFile(file);
 
-        struct coordinate lastPoint;
-	while(fscanf(file, "%c%d,", &c, &i)>0) {
-                if(firstCable == 1) {
-                        if(c != '\n'){
-                        	lastPoint = calculateNewCoordinates(cablePath, c, i, steps, lastPoint);
-			} else {
-                                firstCable = 0;
-                        }
-                }
-        }
- 	
-	return 0;	
+	return closestIntersect;	
 }
 
 int main() {
